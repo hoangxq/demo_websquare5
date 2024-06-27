e17bdfa2cc55df9cad2fa32f5ded0051607fb19f
hoangxq <holmes.arther@gmail.com>
2024-06-28 00:38:21 +0700

    update fake data

--- src/main/java/com/demo/resource/UserResource.java
+++ src/main/java/com/demo/resource/UserResource.java
@@ -1,12 +1,13 @@
 package com.demo.resource;
 
-import com.demo.dto.UserDto;
 import com.demo.dto.request.RemoveUsersReq;
 import com.demo.dto.request.UpsertUserReq;
 import com.demo.dto.request.UserCriteria;
 import com.demo.dto.utils.PagingReq;
 import com.demo.dto.utils.ResponseUtils;
+import com.demo.entity.Team;
 import com.demo.entity.User;
+import com.demo.fake_data.UserInfoGenerator;
 import com.demo.repository.TeamRepository;
 import com.demo.repository.UserRepository;
 import com.demo.service.UserService;
@@ -14,6 +15,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @RestController
@@ -45,35 +54,35 @@ public class UserResource {
     public ResponseEntity<?> exportDataUsers(@RequestBody(required = false) UserCriteria userCriteria) {
         return ResponseUtils.ok(userService.exportDataUsers(userCriteria));
     }
-//
-//    @GetMapping("/fake-data")
-//    public ResponseEntity<?> fakeData() throws IOException {
-//        List<User> users = new ArrayList<>();
-//
-//        LocalDate start = LocalDate.of(1989, 1, 1);
-//        LocalDate end = LocalDate.of(2004, 1, 1);
-//
-//        for (int i = 0; i < 300; i++) {
-//            String fullName = UserInfoGenerator.generateFullName();
-//            users.add(new User(
-//                    null,
-//                    fullName,
-//                    fullName.replaceAll(" ", "").replaceAll("[^\\x20-\\x7e]", "") + UserInfoGenerator.generateRandomEmail(),
-//                    Date.from(UserInfoGenerator.between(start, end).atStartOfDay(ZoneId.systemDefault()).toInstant()),
-//                    UserInfoGenerator.generateRandomGender(),
-//                    UserInfoGenerator.generateRandomPhoneNumber(),
-//                    UserInfoGenerator.generateRandomAddress(),
-//                    generateRandomTeam(),
-//                    UserInfoGenerator.generateRandomStatus()
-//            ));
-//        }
-//        userRepository.saveAll(users);
-//        return ResponseUtils.ok(users);
-//    }
-//
-//    private Team generateRandomTeam() {
-//        List<Team> teams = teamRepository.findAll();
-//        Random random = new Random();
-//        return teams.get(random.nextInt(teams.size()));
-//    }
+
+    @GetMapping("/fake-data")
+    public ResponseEntity<?> fakeData() throws IOException {
+        List<User> users = new ArrayList<>();
+
+        LocalDate start = LocalDate.of(1989, 1, 1);
+        LocalDate end = LocalDate.of(2004, 1, 1);
+
+        for (int i = 0; i < 300; i++) {
+            String fullName = UserInfoGenerator.generateFullName();
+            users.add(new User(
+                    null,
+                    fullName,
+                    fullName.replaceAll(" ", "").replaceAll("[^\\x20-\\x7e]", "") + UserInfoGenerator.generateRandomEmail(),
+                    Date.from(UserInfoGenerator.between(start, end).atStartOfDay(ZoneId.systemDefault()).toInstant()),
+                    UserInfoGenerator.generateRandomGender(),
+                    UserInfoGenerator.generateRandomPhoneNumber(),
+                    UserInfoGenerator.generateRandomAddress(),
+                    generateRandomTeam(),
+                    UserInfoGenerator.generateRandomStatus()
+            ));
+        }
+        userRepository.saveAll(users);
+        return ResponseUtils.ok(users);
+    }
+
+    private Team generateRandomTeam() {
+        List<Team> teams = teamRepository.findAll();
+        Random random = new Random();
+        return teams.get(random.nextInt(teams.size()));
+    }
 }
