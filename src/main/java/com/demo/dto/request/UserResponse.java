e14289b59598619fed18faa2593bb85257aded99
AtomicOrbital <topthuthuat69@gmail.com>
2024-06-27 17:25:36 +0700

    complete

--- src/main/java/com/demo/dto/request/UserResponse.java
+++ src/main/java/com/demo/dto/request/UserResponse.java
@@ -1,5 +1,6 @@
 package com.demo.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -9,10 +10,12 @@ public class UserRequest {
     private Integer id;
     private String name;
     private String email;
-    private Date birthDate;
     private String gender;
     private String phone;
     private String address;
     private String team;
     private String status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date birthDate;
 }
